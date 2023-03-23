2d0b3760e9c97365295c1982b2c965bd5a9cf553
Junaid Ahmed <123470736+junaidwh@users.noreply.github.com>
2023-03-23 18:15:09 +0500

    Add files via upload

---
+++ src/javaproject/JavaProject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaproject;
+
+/**
+ *
+ * @author one
+ */
+public class JavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Login log =new Login();
+        log.show();
+    }
+}
